cc26995d31c4603461dfad35fd0d971eb63ef9fe
dam1 <dam1@1DAM-14.ies.quevedo>
2018-02-27 10:10:43 +0100

    Prueba Git

---
+++ Ejercicio Java/src/ejercicio/java/EjercicioJava.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dam1
+ */
+public class EjercicioJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner leer = new Scanner (System.in);
+        int n = 0;
+        do {            
+            n++;
+            System.out.println(n);
+        } while (n != 100);
+    }
+    
+}
